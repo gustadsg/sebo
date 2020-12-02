5e6291a13b495a51554280c72695b2e032731274
Gustavo <gustadsg@outlook.com.br>
2020-12-02 20:14:40 -0300

    adding routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express'); //serve para criar rotas
 const cors = require('cors'); //serve para requisições seguras e transferências de dados
+const routes = require('./routes'); //rotas do sistema
 
 const port = process.env.PORT || 3333;
 
@@ -7,6 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json()); //serve para poder interpretar json
+
+app.use(routes);
 
 app.listen(port, ()=>{
     console.log(`server listening on port ${port}`)
