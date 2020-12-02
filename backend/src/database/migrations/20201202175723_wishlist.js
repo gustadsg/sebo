1d32991f11e7fe451e1b00485abd8ffc0d62fda2
Gustavo <gustadsg@outlook.com.br>
2020-12-02 18:42:27 -0300

    migration updated

--- backend/src/database/migrations/20201202175723_wishlist.js
+++ backend/src/database/migrations/20201202175723_wishlist.js
@@ -3,9 +3,9 @@ exports.up = function(knex) {
   return knex.schema.createTable('wishlist', function(table){
       table.increments('wish_id').primary().notNullable();
       table.string('user_id').notNullable();
-      table.foreign('user_id').references('user_id').inTable('user');
+      table.foreign('user_id').references('user_id').inTable('user').onDelete('cascade').onUpdate('cascade');
       table.string('book_id').notNullable();
-      table.foreign('book_id').references('book_id').inTable('book');
+      table.foreign('book_id').references('book_id').inTable('book').onDelete('cascade').onUpdate('cascade');
   });
 };
 
