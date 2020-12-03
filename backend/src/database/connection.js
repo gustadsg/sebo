30a20da930791885604ff6a78f935ac0ac95e2ed
Gustavo <gustadsg@outlook.com.br>
2020-12-03 10:43:27 -0300

    formatting

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,6 @@
-const enviroment = process.env.NODE_ENV || 'development';
-const configuration = require('../../knexfile')[enviroment];
+const enviroment = process.env.NODE_ENV || "development";
+const configuration = require("../../knexfile")[enviroment];
 console.log(configuration);
 
-const connection = require('knex')(configuration);
+const connection = require("knex")(configuration);
 module.exports = connection;
--- backend/src/middlewares/validators/Wishlist.js
+++ backend/src/middlewares/validators/Wishlist.js
@@ -1,33 +1,32 @@
-const {celebrate, Joi, Segments } = require('celebrate');
-
+const { celebrate, Joi, Segments } = require("celebrate");
 
 module.exports = {
-    create: celebrate({
-        [Segments.PARAMS] : Joi.object().keys({
-            user_id: Joi.string().required()
-        }),
-        [Segments.BODY]: Joi.object().keys({
-            book_id: Joi.number().integer().required()
-          })
+  create: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      user_id: Joi.string().required(),
     }),
-    getById: celebrate({
-        [Segments.PARAMS] : Joi.object().keys({
-            wish_id: Joi.string().required()
-        })
+    [Segments.BODY]: Joi.object().keys({
+      book_id: Joi.number().integer().required(),
     }),
-    getByUserId: celebrate({
-        [Segments.PARAMS] : Joi.object().keys({
-            user_id: Joi.string().required()
-        })
+  }),
+  getById: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      wish_id: Joi.string().required(),
     }),
-    getByBookId: celebrate({
-        [Segments.PARAMS] : Joi.object().keys({
-            book_id: Joi.string().required()
-        })
+  }),
+  getByUserId: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      user_id: Joi.string().required(),
     }),
-    deleteById: celebrate({
-        [Segments.PARAMS]: Joi.object().keys({
-            wish_id: Joi.string().required()
-        })
-    })
-}
+  }),
+  getByBookId: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      book_id: Joi.string().required(),
+    }),
+  }),
+  deleteById: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      wish_id: Joi.string().required(),
+    }),
+  }),
+};
