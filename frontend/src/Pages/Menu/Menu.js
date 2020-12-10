caf58b94c132cb5a84cde74ac3e7ebb61b225479
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2020-12-10 09:24:35 -0300

    created drop-menu with logout

--- frontend/src/Pages/Menu/Menu.js
+++ frontend/src/Pages/Menu/Menu.js
@@ -1,103 +1,135 @@
-import { Drawer,
-        List, 
-        ListItem, 
-        ListItemText, 
-        Typography, 
-        AppBar, Toolbar, 
-        IconButton, 
-        Avatar } from "@material-ui/core";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Avatar,
+} from "@material-ui/core";
 import React, { useState } from "react";
-import { MdHome, MdLibraryBooks, MdMenu, MdLaptop } from "react-icons/md"
-import { FaUser } from "react-icons/fa"
+import { MdHome, MdLibraryBooks, MdMenu, MdLaptop } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import "./Menu.css";
 import { useHistory } from "react-router-dom";
 
+function Menu(props) {
+  const history = useHistory();
+  const [currentPage, setCurrentPage] = useState("/home");
+  const [open, setOpen] = useState(false);
 
-function Menu(props){
-    const history = useHistory();
-    const [currentPage, setCurrentPage] = useState("/home");
-    const [open, setOpen] = useState(false);
+  function toggleAvatarMenu() {
+    const avatarMenu = document.querySelector("#avatar-menu");
+    avatarMenu.classList.contains("hidden")
+      ? avatarMenu.classList.remove("hidden")
+      : avatarMenu.classList.add("hidden");
+  }
 
-    function handleClick(pathName){
-        history.push(pathName);
-        setCurrentPage(pathName);
-    }
+  function handleClick(pathName) {
+    history.push(pathName);
+    setCurrentPage(pathName);
+  }
 
-    function handleDrawer(isOpen){
-        setOpen(isOpen);
-    }
+  function handleDrawer(isOpen) {
+    setOpen(isOpen);
+  }
 
-    const pages = [
-        {
-            pathName: "/home",
-            icon: <MdHome/>,
-            text: "Home",
-            iconSize: "1.5em",
-        },
-        {
-            pathName: "/nossoslivros",
-            icon: <MdLibraryBooks/>,
-            text: "Nossos Livros",
-            iconSize: "1.5em",
-        },
-        {
-            pathName: "/login",
-            icon: <FaUser/>,
-            text: "Login",
-            iconSize: "1.5em",
-        },
-        {
-            pathName: "/cadastro",
-            icon: <MdLaptop/>,
-            text: "Cadastro",
-            iconSize: "1.7em",
-        },
-    ];
+  const pages = [
+    {
+      pathName: "/home",
+      icon: <MdHome />,
+      text: "Home",
+      iconSize: "1.5em",
+    },
+    {
+      pathName: "/nossoslivros",
+      icon: <MdLibraryBooks />,
+      text: "Nossos Livros",
+      iconSize: "1.5em",
+    },
+    {
+      pathName: "/login",
+      icon: <FaUser />,
+      text: "Login",
+      iconSize: "1.5em",
+    },
+    {
+      pathName: "/cadastro",
+      icon: <MdLaptop />,
+      text: "Cadastro",
+      iconSize: "1.7em",
+    },
+  ];
 
-    return (
-        <>
-        <AppBar position="fixed">
-            <Toolbar className="toolBar">
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className="toolBar">
+          <IconButton
+            edge="start"
+            aria-label="menu"
+            onClick={() => handleDrawer(!open)}
+          >
+            <MdMenu />
+          </IconButton>
 
-                <IconButton edge="start" aria-label="menu" onClick={() => handleDrawer(!open)}>
-                    <MdMenu />
-                </IconButton>
+          <div className="logo">
+            <img
+              className="ico"
+              alt="imagem"
+              src="/images/HaiKaiVetorized.png"
+            />
+          </div>
 
-                <div className="logo">
-                    <img className="ico" alt="imagem" src="/images/HaiKaiVetorized.png"/>
+          <div className="userContainer">
+            <Avatar
+              alt="User"
+              className="avatar"
+              src="/images/user.png"
+              onClick={toggleAvatarMenu}
+            />
+          </div>
+
+          <div id="avatar-menu">
+            <ul className="drop-menu">
+              <li className="drop-menu-item">
+                <div>
+                  <button className="logout-btn">Logout</button>
                 </div>
+              </li>
+            </ul>
+          </div>
+        </Toolbar>
+      </AppBar>
 
-                <div className="userContainer">
-                    <Avatar alt="User" src="/images/user.png" />
-                </div>
-
-            </Toolbar>
-        </AppBar>
-
-        {props.children}
-        <Drawer open = {open} onClose = {() => handleDrawer(false)}>
-            <List className="list">
-                {pages.map((listItem) => {
-                    return (
-                        <ListItem 
-                        button
-                        selected={currentPage === listItem.pathName}
-                        onClick={
-                            () => handleClick(listItem.pathName)}>
-                            <IconContext.Provider value={{size: listItem.iconSize}}>
-                                {listItem.icon}
-                            </IconContext.Provider>
-                            <ListItemText className="listItemText">
-                                <Typography className="typography">{listItem.text}</Typography>
-                            </ListItemText>
-                        </ListItem>
-                    );
-                })}
-            </List>
-        </Drawer>
-        </>
-    );
+      {props.children}
+      <Drawer open={open} onClose={() => handleDrawer(false)}>
+        <List className="list">
+          {pages.map((listItem) => {
+            return (
+              <ListItem
+                button
+                selected={currentPage === listItem.pathName}
+                onClick={() => handleClick(listItem.pathName)}
+              >
+                <IconContext.Provider value={{ size: listItem.iconSize }}>
+                  {listItem.icon}
+                </IconContext.Provider>
+                <ListItemText className="listItemText">
+                  <Typography className="typography">
+                    {listItem.text}
+                  </Typography>
+                </ListItemText>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Drawer>
+    </>
+  );
 }
 
 export default Menu;
