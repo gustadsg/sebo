327b69a37ee0239368ac8ee22b6662f00820a17e
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2020-12-14 21:21:51 -0300

    register user page integrated with backend

--- frontend/src/Pages/Cadastro/Cadastro.js
+++ frontend/src/Pages/Cadastro/Cadastro.js
@@ -4,20 +4,45 @@ import { useHistory } from "react-router-dom";
 import { Form, FormGroup } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
+import api from '../../services/backend'
 require("typeface-quicksand");
 
 function Cadastro() {
   const history = useHistory();
-  const [nome, setNome] = useState();
+  const [state, setstate] = useState();
 
-  function Home() {
-    alert("Bem vindo " + nome);
-    history.push("Home");
+  function handleChange(e) {
+    const oldState = state;
+    setstate({...oldState, [e.target.name]: e.target.value})
   }
+  
+  function passwordMatchesConfirmation(){
+    if(!state['passwordConfirmation']) alert('Digite a confirmação da senha para prosseguir');
+    return state['password'] === state['passwordConfirmation']
+  }
+
+  function handleSubmit(){
+    if (!state) return alert('preencha o formulário para se cadastrar')
+    if(!passwordMatchesConfirmation()) {
+      alert('Senha e Confirmação são diferentes')
+      return;
+    }
+    try {
+      const data = state;
+      delete data['passwordConfirmation']
+      console.log(data)
+      api.post('/users', data).then((res)=>{
+        alert('Usuário criado com sucesso!');
+      });
+    } catch (err) {
+      alert('Não foi possível cadastrar usuário.')
+    }
+  }
+
   return (
     <div className="Cadastro">
       <div className="button">
-        <Button className="butao" variant="danger" onClick={Home}>
+        <Button className="butao" variant="danger" onClick={handleSubmit}>
           Cadastrar!{" "}
         </Button>
       </div>
@@ -37,13 +62,14 @@ function Cadastro() {
                 ></img>
                 <div className="dados1aux">
                   <Form className="dados1">
-                    <Form.Group controlId="nome">
+                    <Form.Group controlId="name">
                       <Form.Label>Nome</Form.Label>
                       <Form.Control
                         plaintext
+                        name="name"
                         size="sm"
                         placeholder="Seu nome"
-                        onChange={(e) => setNome(e.target.value)}
+                        onChange={handleChange}
                       />
                     </Form.Group>
 
@@ -51,27 +77,33 @@ function Cadastro() {
                       <Form.Label>Email</Form.Label>
                       <Form.Control
                         plaintext
+                        name='email'
                         size="sm"
+                        onChange={handleChange}
                         placeholder="name@example.com.br"
                       />
                     </Form.Group>
 
-                    <Form.Group controlId="senha">
+                    <Form.Group controlId="password">
                       <Form.Label>Senha</Form.Label>
                       <Form.Control
                         type="password"
+                        name="password"
                         plaintext
                         size="sm"
+                        onChange={handleChange}
                         placeholder="Senha"
                       />
                     </Form.Group>
 
-                    <Form.Group controlId="senhaconf">
+                    <Form.Group controlId="passwordConfirmation">
                       <Form.Label>Confirme sua senha</Form.Label>
                       <Form.Control
                         type="password"
+                        name="passwordConfirmation"
                         plaintext
                         size="sm"
+                        onChange={handleChange}
                         placeholder="Senha"
                       />
                     </Form.Group>
