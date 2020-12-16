92944283c9158d9cb390e1bb25d8fe9d103c0539
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2020-12-16 09:55:33 -0300

    bookEdit ready

--- backend/src/middlewares/validators/Book.js
+++ backend/src/middlewares/validators/Book.js
@@ -21,13 +21,16 @@ module.exports = {
     [Segments.PARAMS]: Joi.object().keys({
       book_id: Joi.string().required(),
     }),
-    [Segments.BODY]: Joi.object().keys({
-      title: Joi.string().min(3).required(),
-      image_path: Joi.string(),
-      description: Joi.string().required(),
-      author: Joi.string().default("unknown"),
-      quantity: Joi.number().integer(),
-    }),
+    [Segments.BODY]: Joi.object()
+      .keys({
+        title: Joi.string().min(3).required(),
+        image_path: Joi.string(),
+        description: Joi.string().required(),
+        author: Joi.string().default("unknown"),
+        quantity: Joi.number().integer(),
+        sale: Joi.number().integer().default(0),
+      })
+      .unknown(),
   }),
   deleteById: celebrate({
     [Segments.PARAMS]: Joi.object().keys({
